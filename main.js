61f32cdccacfbf7b91fcdba0a738bdca9c92b7d4
Ahmad Daffa Dhaifullah <47172711+ahmad5944@users.noreply.github.com>
2023-02-09 06:12:34 +0700

    update keydown

--- main.js
+++ main.js
@@ -4,3 +4,9 @@ const inputColor = document.getElementById("inputColor");
 function submit() {
     cardBackgroundColor.style.backgroundColor = inputColor.value;
 }
+
+inputColor.addEventListener("keydown", function (e) {
+    if (e.code === "ControlLeft" || e.code === "Enter") {
+        submit();
+    }
+});
